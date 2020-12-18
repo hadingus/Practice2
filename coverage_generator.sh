b8f2de7546cb23a0a416fbfd6234d544104c7798
Eugene Tischenko <mr.hadingus@gmail.com>
2020-12-19 00:44:51 +0500

    update coverage

--- coverage_generator.sh
+++ coverage_generator.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rm -r build
 mkdir build
 cd build
 cmake ..
 make
 ./Tests
+cd ..
 
-lcov --directory CMakeFiles/Tests.dir/ --capture --output-file ./code_coverage.info -rc lcov_branch_coverage=1
-genhtml code_coverage.info --branch-coverage --output-directory ./code_coverage_report/
+lcov --directory ./build/CMakeFiles/Tests.dir/ --capture --output-file ./coverage.info -rc lcov_branch_coverage=1
+genhtml coverage.info --branch-coverage --output-directory ./code_coverage_report/
